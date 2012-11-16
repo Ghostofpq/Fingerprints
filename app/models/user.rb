14f374bf31c9bdd8f84100f77159aface82cfe44
GhostOfPQ <ghostofpq@gmail.com>
2012-11-16 17:10:22 +0100

    mots de passe ajout??s en theorie

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,21 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
-  
+  attr_accessible :name, :email, :password, :password_confirmation
+  has_secure_password
+ 
   before_save { |user| user.email = email.downcase }
   
   validates :name,  presence: true, length: { maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true
+  validates :email, presence:     true,
+                    format:       { with: VALID_EMAIL_REGEX },
+                    uniqueness:   true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  validates_confirmation_of :password
+  
+  
+  
+  
   
   has_many :microposts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121116155829) do
+ActiveRecord::Schema.define(:version => 20121116160254) do
 
   create_table "microposts", :force => true do |t|
     t.integer  "user_id"
@@ -23,8 +23,9 @@ ActiveRecord::Schema.define(:version => 20121116155829) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
