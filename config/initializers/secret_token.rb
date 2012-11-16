d32157066568dc24f7ef98a7166032e7723a0322
GhostOfPQ <ghostofpq@gmail.com>
2012-11-16 15:11:32 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fingerprints::Application.config.secret_token = '3dc263409933aba3dbf794a2e74f6f320984f269e83ea872b91f2cab2e834296ee6d35d1a09c9fa7b0a27178ab5e34374b4e80a9d00e56d01a57c90b89a04460'
