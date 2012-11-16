a3611024d80cdc626d3d13b51703dc9bff3cf09f
GhostOfPQ <ghostofpq@gmail.com>
2012-11-16 16:16:20 +0100

    ajout des relations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,7 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :desc, :user_id
+
+  belongs_to :user
+
+  validates :content, :length => { :maximum => 140 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121116144757) do
+ActiveRecord::Schema.define(:version => 20121116151030) do
+
+  create_table "microposts", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "desc"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
