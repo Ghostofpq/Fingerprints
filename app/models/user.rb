c3afcca84a417d3c03ed2c6a4d5eebed09332e87
GhostOfPQ <ghostofpq@gmail.com>
2012-11-19 17:21:20 +0100

    on ajoute les sessions et un remember token

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,13 @@
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
 #
-
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
- 
+
   before_save { |user| user.email = email.downcase }
-  
+  before_save :create_remember_token
+
   validates :name,  presence: true, length: { maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:     true,
@@ -23,5 +23,10 @@ class User < ActiveRecord::Base
                     uniqueness:   true
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
-  validates_confirmation_of :password  
+  validates_confirmation_of :password
+
+  private
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121116160254) do
+ActiveRecord::Schema.define(:version => 20121119161641) do
 
   create_table "microposts", :force => true do |t|
     t.integer  "user_id"
@@ -26,8 +26,10 @@ ActiveRecord::Schema.define(:version => 20121116160254) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
