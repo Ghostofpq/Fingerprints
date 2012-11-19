5e6e402c4af4e51889af6f24a07d7e873e70eae3
Ghostofpq <ghostofpq@gmail.com>
2012-11-19 21:25:44 +0100

    signin signout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,8 +32,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-      flash[:success] = "Welcome to Fingerprints!"
     else
       render 'new'
     end
