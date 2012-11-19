408c16f50c721fc062d130a83cea161257497fef
GhostOfPQ <ghostofpq@gmail.com>
2012-11-19 14:05:36 +0100

    fin de la partie 5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,12 +24,12 @@ class UsersController < ApplicationController
   # GET /users/new
   # GET /users/new.json
   def new
-    @user = User.new
+   # @user = User.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
+   # respond_to do |format|
+   #   format.html # new.html.erb
+   #   format.json { render json: @user }
+   # end
   end
 
   # GET /users/1/edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Fingerprints::Application.routes.draw do
+  get "users/new"
+  
   root to: 'static_pages#home'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+
+  match '/signup',    to: 'users#new'
 
   resources :microposts
 
