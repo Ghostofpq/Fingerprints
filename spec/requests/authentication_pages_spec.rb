c22695cd940e64774ec5abd39590d26113bc0ce9
GhostOfPQ <ghostofpq@gmail.com>
2012-11-20 13:32:02 +0100

    refactoring du formulaire

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -3,14 +3,9 @@ require 'spec_helper'
 describe "Authentication" do
 
   subject { page }
-
-  describe "signin page" do
-    before { visit signin_path }
-
-    it { should have_selector('h1',    text: 'Sign in') }
-    it { should have_selector('title', text: 'Sign in') }
-  end
-
+  #========================
+  #  SIGNIN 
+  #========================
   describe "signin" do
     before { visit signin_path }
 
@@ -25,29 +20,37 @@ describe "Authentication" do
         it { should_not have_selector('div.alert.alert-error') }
       end
     end
-
+  
+  #========================
+  #  SIGNIN PAGE
+  #========================
+  describe "signin page" do
+    before { visit signin_path }
+    it { should have_selector('h1',    text: 'Sign in') }
+    it { should have_selector('title', text: 'Sign in') }
+  end
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before { sign_in user }
-
-      it { should have_selector('title', text: user.name) }
-      
+      it { should have_selector('title', text: user.name) }  
       it { should have_link('Users',    href: users_path) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
-      
       it { should_not have_link('Sign in', href: signin_path) }
-
       describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
       end
     end
   end
-
+  #========================
+  #  AUTHORIZATION
+  #========================
   describe "authorization" do
-
+    #========================
+    #  non-signed-in users
+    #========================
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       describe "when attempting to visit a protected page" do
@@ -77,8 +80,35 @@ describe "Authentication" do
           specify { response.should redirect_to(signin_path) }
         end
       end
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+        describe "after signing in" do
+          it "should render the desired protected page" do
+            page.should have_selector('title', text: 'Edit user')
+          end
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+            it "should render the default (profile) page" do
+              page.should have_selector('title', text: user.name) 
+            end
+          end
+        end
+      end
     end
-    
+    #========================
+    #  wrong user
+    #========================
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
       let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
@@ -92,13 +122,13 @@ describe "Authentication" do
         specify { response.should redirect_to(root_path) }
       end
     end
-    
+    #========================
+    #  non-admin user
+    #========================        
     describe "as non-admin user" do
       let(:user) { FactoryGirl.create(:user) }
       let(:non_admin) { FactoryGirl.create(:user) }
-
       before { sign_in non_admin }
-
       describe "submitting a DELETE request to the Users#destroy action" do
         before { delete user_path(user) }
         specify { response.should redirect_to(root_path) }        
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,34 +1,28 @@
 require 'spec_helper'
 
 describe "User pages" do
-
   subject { page }
-
+  #========================
+  #  INDEX
+  #========================
   describe "index" do
+    let(:user) { FactoryGirl.create(:user) }
     before do
-      sign_in FactoryGirl.create(:user)
-      FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
-      FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+      sign_in user
       visit users_path
     end
-
     it { should have_selector('title', text: 'All users') }
     it { should have_selector('h1',    text: 'All users') }
-
     describe "pagination" do
-
       before(:all) { 30.times { FactoryGirl.create(:user) } }
       after(:all)  { User.delete_all }
-
       it { should have_selector('div.pagination') }
-
       it "should list each user" do
         User.paginate(page: 1).each do |user|
           page.should have_selector('li', text: user.name)
         end
       end
-    end 
-    
+    end
     describe "delete links" do
       it { should_not have_link('delete') }
       describe "as an admin user" do
@@ -43,81 +37,59 @@ describe "User pages" do
         end
         it { should_not have_link('delete', href: user_path(admin)) }
       end
-    end   
+    end
   end
-
-  describe "signup page" do
-    before { visit signup_path }
-
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
-  end
-
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
-
-    it { should have_selector('h1',    text: user.name) }
-    it { should have_selector('title', text: user.name) }
-  end
-
+  #========================
+  #  SIGNUP
+  #========================
   describe "signup" do
     before { visit signup_path }
     let(:submit) { "Create my account" }
-
     describe "with invalid information" do
-
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
-
       describe "after submission" do
         before { click_button submit }
-
         it { should have_selector('title', text: 'Sign up') }
         it { should have_content('error') }
       end
     end
-
     describe "with valid information" do
       before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Name", with: "Example User"
+        fill_in "Email",with: "user@example.com"
+        fill_in "Password",with: "foobar"
+        fill_in "Confirm Password",with: "foobar"
       end
-
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
-
       describe "after saving the user" do
         before { click_button submit }
         let(:user) { User.find_by_email('user@example.com') }
-
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
         it { should have_link('Sign out') }
       end
     end
   end
-
+  #========================
+  #  EDIT
+  #========================
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
     before do
       sign_in user
       visit edit_user_path(user)
     end
-    
     describe "page" do
       it { should have_selector('h1',    text: "Update your profile") }
       it { should have_selector('title', text: "Edit user") }
       it { should have_link('change', href: 'http://gravatar.com/emails') }
     end
-
     describe "with invalid information" do
       before { click_button "Save changes" }
-
       it { should have_content('error') }
     end
     describe "with valid information" do
@@ -136,5 +108,22 @@ describe "User pages" do
       specify { user.reload.name.should  == new_name }
       specify { user.reload.email.should == new_email }
     end
+  end
+  #========================
+  #  SIGNUP PAGE
+  #========================
+  describe "signup page" do
+    before { visit signup_path }
+    it { should have_selector('h1',    text: 'Sign up') }
+    it { should have_selector('title', text: full_title('Sign up')) }
+  end
+  #========================
+  #  PROFILE PAGE
+  #========================
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
   end
 end
