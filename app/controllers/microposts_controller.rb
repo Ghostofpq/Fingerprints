156cb6732a9fe702185e08c547b883a271887b3a
GhostOfPQ <ghostofpq@gmail.com>
2012-11-21 16:15:57 +0100

    on peut creer des mp directement publics

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -6,6 +6,7 @@ class MicropostsController < ApplicationController
 
   def create
     @micropost = current_user.microposts.build(params[:micropost])
+    @micropost.public=params[:public]
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_url
