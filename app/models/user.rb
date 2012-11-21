0a3dc7073e4a8574b26d880925bfe4a8db1d9d5d
GhostOfPQ <ghostofpq@gmail.com>
2012-11-21 10:50:19 +0100

    c'est fini pour le tuto

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,7 @@ class User < ActiveRecord::Base
   validates_confirmation_of :password
 
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
   
   def following?(other_user)
