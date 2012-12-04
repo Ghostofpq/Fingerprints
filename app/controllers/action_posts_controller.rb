7a6bfd9043798f513dc089ed158ce42da84bfea8
GhostOfPQ <ghostofpq@gmail.com>
2012-12-04 17:06:52 +0100

    ajout du edit sur les actions

--- app/controllers/action_posts_controller.rb
+++ app/controllers/action_posts_controller.rb
@@ -1,4 +1,6 @@
 class ActionPostsController < ApplicationController
+  before_filter :signed_in_user, only: [:create, :destroy]
+  before_filter :correct_user,   only: [:destroy,:set_public,:set_private,:edit]
   def index
     @action_posts = ActionPost.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Fingerprints::Application.routes.draw do
   resources :sessions,      only: [:new, :create, :destroy]
   resources :microposts,    only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
-  resources :action_posts
+  resources :action_posts, only: [:create, :destroy, :edit]
   
   root to: 'static_pages#home'
 
