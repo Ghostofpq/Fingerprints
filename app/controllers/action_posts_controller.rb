2ca9ed2b5bb28e22f3e1e3ede8cf59bc324ae795
GhostOfPQ <ghostofpq@gmail.com>
2012-12-05 16:46:16 +0100

    on peut editer les dates des actions <3

--- app/controllers/action_posts_controller.rb
+++ app/controllers/action_posts_controller.rb
@@ -32,13 +32,34 @@ class ActionPostsController < ApplicationController
 
   def update
     @action_post_u = ActionPost.find(params[:id])
-    @action_post_u.update_attribute(:comment,params[:action_post][:comment])
-    @action_post_u.update_attribute(:place,params[:action_post][:place])
-    @action_post_u.update_attribute(:score,params[:action_post][:score])
-    @action_post_u.update_attribute(:price,params[:action_post][:price])
 
-    redirect_to root_url
+    @date1= DateTime.new(params[:action_post]["start_date(1i)"].to_i,
+                        params[:action_post]["start_date(2i)"].to_i,
+                        params[:action_post]["start_date(3i)"].to_i,
+                        params[:action_post]["start_date(4i)"].to_i,
+                        params[:action_post]["start_date(5i)"].to_i)
+    @date2= DateTime.new(params[:action_post]["end_date(1i)"].to_i,
+                        params[:action_post]["end_date(2i)"].to_i,
+                        params[:action_post]["end_date(3i)"].to_i,
+                        params[:action_post]["end_date(4i)"].to_i,
+                        params[:action_post]["end_date(5i)"].to_i)
 
+    if @date1.future? or @date2.future?
+      flash[:error] = "You've seen the future? Tell me more about that."
+      redirect_to root_url
+    elsif @date1 > @date2
+      flash[:error] = "You're fucking the space time continuum aren't you?"
+      redirect_to root_url
+    else
+      @action_post_u.update_attribute(:start_date,@date1)
+      @action_post_u.update_attribute(:end_date,@date2)
+      @action_post_u.update_attribute(:comment,params[:action_post][:comment])
+      @action_post_u.update_attribute(:place,params[:action_post][:place])
+      @action_post_u.update_attribute(:score,params[:action_post][:score])
+      @action_post_u.update_attribute(:price,params[:action_post][:price])
+
+      redirect_to root_url
+    end
   end
 
   def destroy
