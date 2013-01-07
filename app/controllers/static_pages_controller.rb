79c3607e446e8559d55ddbabaa6432752951a126
GhostOfPQ <ghostofpq@gmail.com>
2013-01-07 10:52:50 +0100

    ogranise en desc

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,7 @@ class StaticPagesController < ApplicationController
       @feed_items = current_user.feed.paginate(page: params[:page],per_page: 5)
       @feed_action_items = current_user.action_feed.paginate(page: params[:page],per_page: 5)
       @global_feed_items=current_user.feed+current_user.action_feed
-      @global_feed_items.sort_by! do |obj| obj.date_sort end
+      @global_feed_items.sort {|obj1, obj2| obj2.date_sort <=> obj1.date_sort}
     end
   end
 
